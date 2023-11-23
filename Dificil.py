fc72d3e2f96ebfe5a4b2108d4100dffc90fdbf42
juandavidr7 <juandavidrinconl@gmail.com>
2023-11-23 13:05:10 -0500

    Se agrega la desactivación de las imagenes de las parejas ya encontradas

--- Dificil.py
+++ Dificil.py
@@ -4,16 +4,20 @@ import random
 
 gi.require_version("Gtk", "3.0")
 
-class Form(Gtk.Window):
+class Form_dificil(Gtk.Window):
+
     def __init__(self, form_instance):
         super().__init__(title="Busca mi pareja")
-        self.set_default_size(50, 50)
-        self.set_border_width(1)
+        self.set_default_size(200, 350)
+        self.set_border_width(10)
 
         self.form_instance = form_instance
 
-        self.cartas_numeros = [1, 2, 3, 4, 1, 2, 3, 4]
+        self.cartas_numeros = list(range(1, 9)) * 2
         self.cartas_seleccionadas = []
+        self.intentos_exitosos = 0
+        self.intentos_fallidos = 0
+        
 
         vbox = Gtk.VBox(spacing=2)
         self.add(vbox)
@@ -21,111 +25,117 @@ class Form(Gtk.Window):
         hbox1 = Gtk.Box(spacing=6)
         hbox2 = Gtk.Box(spacing=6)
         hbox3 = Gtk.Box(spacing=6)
+        hbox4 = Gtk.Box(spacing=6)
+        hbox5 = Gtk.Box(spacing=6)
+        
 
         self.entry_numero_carta = Gtk.Entry()
         self.entry_numero_carta.set_text("")
-        hbox3.pack_start(self.entry_numero_carta, True, True, 0)
+        hbox5.pack_start(self.entry_numero_carta, True, True, 0)
 
-        # Aquí comienza la creación de las imagenes 
         self.imagenes = []
-        for i in range(8):
-            # Asignación de cada uno de las imagenes a un espacio de la interfaz
+
+        for i in range(16):
             imagen = Gtk.Image.new_from_file("img/0.jpeg")
             imagen.numero = self.cartas_numeros[i]
-            # Esto es pa que quede organizado bien bonito xd
+
             if i < 4:
                 hbox1.pack_start(imagen, True, True, 0)
-            else:
+            elif i>=4 and i<8:
                 hbox2.pack_start(imagen, True, True, 0)
-            # Y acá se está creando un array, que me va guardar todas las imagenes en imagenes[]
+            elif i>=8 and i<12:
+                hbox3.pack_start(imagen, True, True, 0)
+            else:
+                hbox4.pack_start(imagen, True, True, 0)
+
             self.imagenes.append(imagen)
 
-        # Todo esto es pura creación de botones, vinculación, asociación con las funciones
-        
         vbox.pack_start(hbox1, True, True, 0)
         vbox.pack_start(hbox2, True, True, 0)
         vbox.pack_start(hbox3, True, True, 0)
+        vbox.pack_start(hbox4, True, True, 0)
+        vbox.pack_start(hbox5, True, True, 0)
 
         lbl_imagen = Gtk.Label()
         lbl_imagen.set_text("Pulse en <<Iniciar Juego>> para comenzar")
         vbox.pack_start(lbl_imagen, True, True, 7)
+
+        lbl_victoria = Gtk.Label()
+        lbl_victoria.set_text("Completa todas las parejas posibles, en el menor tiempo posible")
+        vbox.pack_start(lbl_victoria, True, True, 7)
 
         btn_iniciar_juego = Gtk.Button.new_with_label("Iniciar el juego")
         btn_iniciar_juego.connect("clicked", self.on_iniciar_juego, lbl_imagen)
         vbox.pack_start(btn_iniciar_juego, True, True, 0)
 
         btn_seleccionar = Gtk.Button.new_with_label("Seleccionar carta")
-        btn_seleccionar.connect("clicked", self.on_seleccionar_clicked, lbl_imagen)
+        btn_seleccionar.connect("clicked", self.on_seleccionar_clicked, lbl_imagen, lbl_victoria)
         vbox.pack_start(btn_seleccionar, True, True, 0)
-
-        btn_cerrar = Gtk.Button.new_with_label("Cerrar")
-        btn_cerrar.connect("clicked", self.on_close_clicked)
-        vbox.pack_start(btn_cerrar, True, True, 0)
 
         btn_volver = Gtk.Button.new_with_label("Volver")
         btn_volver.connect("clicked", self.on_cerrar_clicked)
         vbox.pack_start(btn_volver, True, True, 0)
 
-    # Función para llamar a las funciones principales e inicie el juego
+        btn_cerrar = Gtk.Button.new_with_label("Cerrar")
+        btn_cerrar.connect("clicked", self.on_close_clicked)
+        vbox.pack_start(btn_cerrar, True, True, 0)
+
     def on_iniciar_juego(self, button, lbl_imagen):
         self.barajar_cartas()
         self.limpiar_cartas(lbl_imagen)
         self.cartas_seleccionadas = []
-        lbl_imagen.set_text("Seleccione dos cartas:")
+        self.intentos_exitosos = 0
+        self.intentos_fallidos = 0
+        
+        lbl_imagen.set_text("Seleccione dos cartas: Intentos: 0")
 
-    # Esta es la función más perra, aquí se hacen varias cosas
-    def on_seleccionar_clicked(self, button, lbl_imagen):
+    def on_seleccionar_clicked(self, button, lbl_imagen, lbl_victoria):
         numero_carta = int(self.entry_numero_carta.get_text())
-        # Verificar que el número ingresado en la "Entry" esté entre 1 y 8
-        if 1 <= numero_carta <= 8:
+
+        if 1 <= numero_carta <= 16:
             posicion_carta = numero_carta - 1
             carta_seleccionada = self.imagenes[posicion_carta]
-            # Acá lo que hace es que va guardando temporalmente la carta seleccionada,
-            # dentro de otro array de cartas seleccionadas para después hacer comparaciones
+
+            
             if carta_seleccionada not in self.cartas_seleccionadas:
                 lbl_imagen.set_text(f"Carta seleccionada: {numero_carta}")
                 carta_seleccionada.set_from_file(f"img/{(self.cartas_numeros[posicion_carta])}.jpeg")
                 self.cartas_seleccionadas.append(carta_seleccionada)
 
                 if len(self.cartas_seleccionadas) == 2:
-                    self.validar_parejas(lbl_imagen)
-                    self.ocultar_cartas_despues_delay(2)  # Cambiado a 1 segundo
-                    
+                    self.validar_parejas(lbl_imagen, lbl_victoria)
+                    self.ocultar_cartas_despues_delay(2)
+
+                    if self.intentos_exitosos == 8:
+                        lbl_victoria.set_text(f"¡Felicidades! Has encontrado todas las parejas en {self.intentos_exitosos} intentos exitosos, {self.intentos_fallidos} intentos fallidos, con un total de {self.intentos_exitosos + self.intentos_fallidos} intentos.")
+                        self.entry_numero_carta.set_sensitive(False)
+
             else:
                 lbl_imagen.set_text("Número de carta no válido o ya seleccionada. Inténtalo de nuevo.")
+                self.intentos_fallidos += 1
+
+
         else:
             lbl_imagen.set_text("Número de carta no válido. Inténtalo de nuevo.")
 
-    # Esta funcion solo baraja las cartas, para que no salga siempre iguales xd
-    def barajar_cartas(self):
-        random.shuffle(self.cartas_numeros)
-        print(self.cartas_numeros)
-        
-
-
-    # Esto es pa que no se vean las cartas como tal
-    def limpiar_cartas(self, lbl_imagen):
-        for carta in self.imagenes:
-            carta.set_from_file("img/0.jpeg")
-            carta.set_sensitive(True)
-        lbl_imagen.set_text("Seleccione dos cartas:")
-
-    # Acá se van a hacer las validaciones con los dos arrays creados previamente
-    def validar_parejas(self, lbl_imagen):
+    def validar_parejas(self, lbl_imagen, lbl_victoria):
         print(self.cartas_seleccionadas)
-        
-        if self.cartas_seleccionadas[0].get_property("file")== self.cartas_seleccionadas[1].get_property("file"):
-            
+
+        if self.cartas_seleccionadas[0].get_property("file") == self.cartas_seleccionadas[1].get_property("file"):
             lbl_imagen.set_text("¡Encontraste pareja!")
+            self.cartas_seleccionadas[0].set_sensitive(False)  
+            self.cartas_seleccionadas[1].set_sensitive(False)
             self.cartas_seleccionadas = []
+            self.intentos_exitosos += 1
+
+            
         else:
             lbl_imagen.set_text("¡No es pareja! Intenta con otra carta.")
+            self.intentos_fallidos += 1
 
-    # Aca se define el tiempo que van a tardar en volverse a ocultar
     def ocultar_cartas_despues_delay(self, delay):
         GLib.timeout_add(delay * 1000, self.ocultar_cartas)
 
-    # Esto es para que las cartas se vuelvan a ocultar, si no se encuentra que sean pareja
     def ocultar_cartas(self):
         for imagen in self.cartas_seleccionadas:
             imagen.set_from_file("img/0.jpeg")
@@ -133,6 +143,18 @@ class Form(Gtk.Window):
         self.cartas_seleccionadas = []
         self.entry_numero_carta.set_text("")
 
+    def barajar_cartas(self):
+        random.shuffle(self.cartas_numeros)
+        print(self.cartas_numeros)
+
+    def limpiar_cartas(self, lbl_imagen):
+        for carta in self.imagenes:
+            carta.set_from_file("img/0.jpeg")
+            carta.set_sensitive(True)
+        lbl_imagen.set_text(f"Seleccione dos cartas: Intentos: {self.intentos_exitosos + self.intentos_fallidos}")
+
+    def on_close_clicked(self, but_cerrar):
+        Gtk.main_quit()
 
     def on_cerrar_clicked(self, widget):
         # Destroy the current window
@@ -141,7 +163,4 @@ class Form(Gtk.Window):
         # Show the form window using the stored reference
         self.form_instance.show_menu()
 
-    # Cerrar el programa
-    def on_close_clicked(self, but_cerrar):
-        Gtk.main_quit()
 
---
+++ niveles.py
@@ -0,0 +1,79 @@
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+from Facil import Form_facil
+from Medio import Form_medio
+from Dificil import Form_dificil
+
+class form_niveles(Gtk.Window):
+    def __init__(self, form_instance):
+        super().__init__(title="Niveles de Juego")
+        self.set_default_size(300, 400)
+        self.set_border_width(10)
+        vb = Gtk.VBox(spacing=2)
+        self.add(vb)
+
+        self.form_instance = form_instance
+
+        hbox1 = Gtk.Box(spacing=6)
+        hbox2 = Gtk.Box(spacing=6)
+        hbox3 = Gtk.Box(spacing=6)
+
+        # Label fila
+        lbl_fila = Gtk.Label()
+        lbl_fila.set_text("Niveles de Juego")
+        vb.pack_start(lbl_fila, True, True, 0)
+
+        # Button cargar valor
+        btn_facil = Gtk.Button.new_with_label("Nivel fácil")
+        btn_facil.connect("clicked", self.nivel_facil)
+        hbox1.pack_start(btn_facil, True, True, 0)
+
+        # Button calcular valor
+        btn_medio = Gtk.Button.new_with_label("Nivel Medio")
+        btn_medio.connect("clicked", self.nivel_medio)
+        hbox2.pack_start(btn_medio, True, True, 0)
+
+        # Button totalizar
+        btn_dificil = Gtk.Button.new_with_label("Nivel Dificil")
+        btn_dificil.connect("clicked", self.nivel_dificil)
+        hbox3.pack_start(btn_dificil, True, True, 0)
+
+        # Se cargan los hbox al vbox
+        vb.pack_start(hbox1, True, True, 0)
+        vb.pack_start(hbox2, True, True, 0)
+        vb.pack_start(hbox3, True, True, 0)
+
+        # Create an attribute to hold the reference to VentanaConfig
+        self.Ventana_Config = None
+
+    def nivel_facil(self, widget):
+        self.Ventana_facil = Form_facil(self)
+        self.Ventana_facil.show_all()
+        self.hide()
+
+    def nivel_medio(self, widget):
+       
+        self.Ventana_medio = Form_medio(self)
+        self.Ventana_medio.show_all()
+        self.hide()
+
+    def nivel_dificil(self, widget):
+        self.Ventana_dificl = Form_dificil(self)
+        self.Ventana_dificl.show_all()
+        self.hide()
+
+    def show_menu(self):
+        self.show_all()
+
+    def on_cerrar_clicked(self, widget):
+        # Destroy the current window
+        self.destroy()
+
+        # Show the form window using the stored reference
+        self.form_instance.show_menu()
+
+
+
+
